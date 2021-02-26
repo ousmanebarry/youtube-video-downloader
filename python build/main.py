504c7e4ebb4a20f85d3f3e336b6d9e3916547c92
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-02-26 14:48:15 -0500

    made directory for downloaded videos

--- python build/main.py
+++ python build/main.py
@@ -1,10 +1,18 @@
 from tkinter import *
 from pytube import YouTube
+import os
 
 root = Tk()
 root.geometry('500x300')
 root.resizable(0, 0)
 root.title("YouTube Downloader")
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+dir_name = 'Downloaded Videos'
+
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
 
 Label(root, text='Youtube Video Downloader', font='arial 20 bold').pack()
 
@@ -15,9 +23,10 @@ link_enter = Entry(root, width=70, textvariable=link).place(x=32, y=90)
 
 
 def Downloader():
+    print(dir_path)
     url = YouTube(str(link.get()))
     video = url.streams.first()
-    video.download('C:/Users/Owner/Videos/Pytube Downloaded Videos/')
+    video.download(dir_path + '\Downloaded Videos')
     Label(root, text='DOWNLOADED', font='arial 15').place(x=180, y=210)
 
 
