9455f6aa24fc8bd64bb2ba8003a75678feadb5ca
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-02-28 03:20:40 -0500

    Update main.py

--- python build/main.py
+++ python build/main.py
@@ -10,8 +10,6 @@ root.title("YouTube Downloader")
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-run = True
-
 dir_name = 'Downloaded Videos'
 
 if not os.path.exists(dir_name):
@@ -26,13 +24,12 @@ link_enter = Entry(root, width=70, textvariable=link).place(x=32, y=90)
 
 
 def Downloader():
-    if run:
-        try:
-            pytube.YouTube(link.get()).streams.get_highest_resolution().download(dir_path + '\Downloaded Videos')
-            subprocess.Popen(f'explorer "{dir_path} \Downloaded Videos"')
-            Label(root, text='DOWNLOADED', font='arial 15').place(x=180, y=210)
-        except pytube.exceptions.RegexMatchError:
-            Label(root, text='No Video Link/ Invalid Video Link', font='arial 15', fg='red').place(x=100, y=220)
+    try:
+        pytube.YouTube(link.get()).streams.get_highest_resolution().download(dir_path + '\Downloaded Videos')
+        subprocess.Popen(f'explorer "{dir_path} \Downloaded Videos"')
+        Label(root, text='DOWNLOADED', font='arial 15').place(x=180, y=210)
+    except pytube.exceptions.RegexMatchError:
+        Label(root, text='No Video Link/ Invalid Video Link', font='arial 15', fg='red').place(x=100, y=220)
 
 
 Button(root, text='DOWNLOAD', font='arial 15 bold', bg='pale violet red', padx=2, command=Downloader).place(x=180,
